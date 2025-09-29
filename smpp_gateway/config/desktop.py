0cc328ca00373d00b779aa06cec1ea21038d0bcd
mariammabele <mariamuj31@gmail.com>
2025-09-29 18:46:37 +0300

    feat: color change

--- smpp_gateway/config/desktop.py
+++ smpp_gateway/config/desktop.py
@@ -4,7 +4,7 @@ def get_data():
 	return [
 		{
 			"module_name": "Smpp Gateway",
-			"color": "grey",
+			"color": "lavender",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Smpp Gateway")
